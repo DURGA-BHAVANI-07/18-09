a3c1396d0fc875bd642060d2ab285e150f87ff90
K.Durga Bhavani <bhavanidurga759@gmail.com>
2024-09-18 22:13:42 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,27 @@
+var father={
+Name:"Sai ram",
+varage:42,
+varoccupation:"centringworker"
+}
+
+var mom={
+    Name:"lakshmi",
+    age:38,
+    occupation:"house wife"
+}
+ var child={
+    me:"bhavani",
+    twosisters:"satya,divya"
+}
+var details=[42,38,"sai ram","lakshmi",{id:1,id:2,id:3},[1,2,3]]
+
+console.log(mom);
+console.log(child);
+console.log(details);
+
+if (a=42) {
+    console.log(true)
+}
+if (b=38) {
+    console.log(false);
+}
